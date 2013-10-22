dfb8453f5ed8c5f1b54b4196dc801ad07dc1e221
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2013-10-22 18:26:22 +0100

    initial commit

---
+++ Assets/Scripts/BoxBehaviour.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var life:int = 1;
+
+function Start () {
+
+}
+
+function Update () {
+  life++;
+}
+
+function OnCollisionEnter(collision:Collision) {
+
+  var obj = collision.gameObject;
+  var objName = collision.gameObject.name;
+  // var sec:int = 4;
+
+  if (objName == "Floor") {
+    destroyer(gameObject, 17);
+  }
+
+}
+
+function destroyer(obj:GameObject, sec:int){
+  var time:int = sec;
+
+
+
+  Destroy(obj, sec);
+}
---
+++ Assets/Scripts/CameraBehaviour.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var target:Transform;
+
+function Start () {
+
+}
+
+function Update () {
+  transform.LookAt(target);
+  transform.Translate(Vector3.right * Time.deltaTime*20);
+}
